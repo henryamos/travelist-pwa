1479eaf56bf7c82a83cfc86c7d1879ecc17d3168
Henry Amos <amoshenry95@gmail.com>
2024-04-25 01:26:29 +0000

    Porgress Web App Functionality image path updated

--- vite.config.js
+++ vite.config.js
@@ -3,31 +3,31 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
 const manifestForPlugIn = {
   registerType:'prompt',
-  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "maskeable_icon.png", "android-chrome-192x192.png","android-chrome-512x512.png","maskable_icon.png"],
   manifest:{
     name:"React-vite-app",
     short_name:"react-vite-app",
     description:"I am a simple vite app",
     icons:[{
-      src: '/android-chrome-192x192.png',
+      src: './public/android-chrome-192x192.png',
       sizes:'192x192',
       type:'image/png',
       purpose:'favicon'
     },
     {
-      src:'/android-chrome-512x512.png',
+      src:'./public/android-chrome-512x512.png',
       sizes:'512x512',
       type:'image/png',
       purpose:'favicon'
     },
     {
-      src: '/apple-touch-icon.png',
+      src: './public/apple-touch-icon.png',
       sizes:'180x180',
       type:'image/png',
       purpose:'apple touch icon',
     },
     {
-      src: '/maskable_icon.png',
+      src: './public/maskable_icon.png',
       sizes:'512x512',
       type:'image/png',
       purpose:'any maskable',
